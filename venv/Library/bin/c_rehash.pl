ba68c9c2dff9af34b946bb2b6ba6202e9c17fb0e
Nidhin <nidhin.chandrasekhar@gmail.com>
2023-07-22 12:42:06 +0530

    setup

--- venv/Library/bin/c_rehash.pl
+++ venv/Library/bin/c_rehash.pl
@@ -13,7 +13,7 @@
 # and add symbolic links to their hash values.
 
 my $dir = "C:\\Program Files\\Common Files\\ssl";
-my $prefix = "C:/Catman/Learning/ML Ops/mlproject/venv\\Library";
+my $prefix = "C:/Catman/Learning/ML Ops/mlproject1/venv\\Library";
 
 my $errorcount = 0;
 my $openssl = $ENV{OPENSSL} || "openssl";
